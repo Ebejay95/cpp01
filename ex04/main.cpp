2848248625582af1400c06ae6df7539e52451fa2
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-10-20 07:30:20 +0200

    sed is for losers

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,22 +6,64 @@
 /*   By: jeberle <jeberle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 11:26:48 by jeberle           #+#    #+#             */
-/*   Updated: 2024/10/18 11:48:31 by jeberle          ###   ########.fr       */
+/*   Updated: 2024/10/20 07:29:08 by jeberle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
+
+std::string findandreplace(std::string str, const std::string& from, const std::string& to) {
+	std::string result;
+	size_t startPos = 0;
+	size_t foundPos;
+	while ((foundPos = str.find(from, startPos)) != std::string::npos) {
+		result.append(str, startPos, foundPos - startPos);
+		result.append(to);
+		startPos = foundPos + from.length();
+	}
+	result.append(str, startPos, std::string::npos);
+	return result;
+}
 
 int	main(int argc, char **argv)
 {
+	std::string fileContent;
+	std::string modified;
+	std::stringstream buffer;
+    std::string filename;
+
 	if (argc != 4)
 	{
-		std::cout << "./sed_is_for_loosers [filename] [s1] [s2]" << std::endl;
+		std::cerr << "./sed_is_for_losers [filename] [s1] [s2]" << std::endl;
 		return (1);
 	}
-	std::ifstream file(argv[1]);
+	filename = argv[1];
+	std::ifstream file(filename);
 	if (!file.good())
-		std::cout << "file " << argv[1] << " not found"<< std::endl;
+	{
+		std::cerr << "file " << filename << " not found"<< std::endl;
+		return (1);
+	}
+	std::cerr << "file " << filename << " found - try to read"<< std::endl;
+	buffer << file.rdbuf();
+	fileContent = buffer.str();
+	file.close();
+	std::cout << std::endl;
+	std::cerr << "File content:\n" << fileContent << std::endl;
+	modified = findandreplace(fileContent, argv[2], argv[3]);
+
+	std::string outFilename = filename + ".replace";
+	std::ofstream outFile(outFilename);
+	if (!outFile.good()) {
+		std::cerr << "Error: Unable to create file " << outFilename << std::endl;
+		return (1);
+	}
+	outFile << modified;
+	outFile.close();
+	std::cout << "\nModified content has been written to the file " << outFilename << std::endl;
+	std::cout << "New files content:\n" << modified << std::endl;
 	return (0);
 }
